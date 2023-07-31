a68f5e580e1ad0f6f29b66c7a601906cb5db8041
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-31 03:21:35 -0600

    50: Tarea - mostrar nombre y número de pokémon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,38 @@
 import { pokeApi } from "@/api";
 import { Layout } from "@/components/layouts";
-import { PokemonListResponse } from "@/interfaces";
+import { PokemonListResponse, SmallPokemon } from "@/interfaces";
 import { Button } from "@nextui-org/react";
 import { GetStaticProps } from "next";
 
-export default function HomePage(props) {
-  console.log(props);
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+export default function HomePage({ pokemons }: Props) {
   return (
     <Layout title="Listado de pokemons">
-      <Button color='gradient'>
-        Hola mundo
-      </Button>
+      <ul>
+        {
+          pokemons.map(({ id, name }) => (
+            <li key={id}>#{id} - {name}</li>
+          ))
+        }
+      </ul>
     </Layout>
   )
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, index) => ({
+    ...pokemon,
+    id: index + 1,
+    img: `https://raw.githubusercontent.com/PokeAPi/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`
+  }));
+
   return {
     props: {
-      pokemons: data.results,
+      pokemons
     },
   }
 };
