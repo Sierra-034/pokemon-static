6cf1a9da52e28ca29929fea5f51ca294a463477c
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-08-02 12:48:00 -0600

    68: Construir el listado de pokémons favoritos

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,20 +1,41 @@
 import { Layout } from "@/components/layouts";
 import { NoFavorites } from "@/components/ui";
 import { localFavorites } from "@/utils";
+import { Card, Grid } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 
 const FavoritesPage = () => {
-    const [favoritePokemons, setFavoritePokemos] = useState<number[]>([]);
+  const [favoritePokemons, setFavoritePokemos] = useState<number[]>([]);
 
-    useEffect(() => {
-        setFavoritePokemos(localFavorites.pokemons());
-    }, []);
-    
-    return (
-        <Layout title="Pokémons - Favoritos">
-            <NoFavorites/>
-        </Layout>
-    )
+  useEffect(() => {
+      setFavoritePokemos(localFavorites.pokemons());
+  }, []);
+
+  return (
+    <Layout title="Pokémons - Favoritos">
+      {
+        favoritePokemons.length === 0
+          ? (<NoFavorites />)
+          : (
+              <Grid.Container gap={2} direction='row' justify='flex-start'>
+                {
+                  favoritePokemons.map(id => (
+                    <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+                      <Card isHoverable isPressable css={{ padding: 10 }}>
+                        <Card.Image
+                          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                          width={'100%'}
+                          height={140}
+                        />
+                      </Card>
+                    </Grid>
+                  ))
+                }
+              </Grid.Container>
+          )
+      }
+    </Layout>
+  )
 };
 
 export default FavoritesPage;
