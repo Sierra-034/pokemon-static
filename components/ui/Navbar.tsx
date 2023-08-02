03f4becc2194e8f734f9a0d42c81c649b71583c0
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-08-01 23:54:03 -0600

    57: Tarea - Navegación y página de favoritos

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,6 @@
-import { Image, Spacer, Text, useTheme } from "@nextui-org/react";
+import Image from 'next/image';
+import NextLink from 'next/link';
+import { Spacer, Text, useTheme, Link } from "@nextui-org/react";
 
 export const Navbar = () => {
     const { theme } = useTheme();
@@ -18,10 +20,14 @@ export const Navbar = () => {
                 width={70}
                 height={70}
             />
-            <Text color="white" h2>P</Text>
-            <Text color="white" h3>okémon</Text>
-            <Spacer css={{ flex: 1 }}/>
-            <Text>Favoritos</Text>
+            <Link href='/' as={NextLink}>
+                <Text color="white" h2>P</Text>
+                <Text color="white" h3>okémon</Text>
+            </Link>
+            <Spacer css={{ flex: 1 }} />
+            <Link href='/favorites' as={NextLink} css={{marginRight: '10px'}}>
+                <Text>Favoritos</Text>
+            </Link>
         </div>
     )
 };
